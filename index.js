a34c7d217f3aa414b3b160aa18c6dbc3e9291901
Théo Lebert <theolebert@air-de-theo.home>
2022-05-30 05:00:19 +0200

    fix remove button issue but some problems with tasksTrack

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Data
 let services = [
-  { name: "Wash Car", price: 10 },
-  { name: "Mow Lawn", price: 20 },
-  { name: "Pull Weeds", price: 30 },
+  { id: 0, name: "Wash Car", price: 10 },
+  { id: 1, name: "Mow Lawn", price: 20 },
+  { id: 2, name: "Pull Weeds", price: 30 },
 ]
 let tasksRequested = []
 let totalAmount = 0
@@ -12,16 +12,34 @@ const washBtn = document.getElementById("wash-btn")
 const mowBtn = document.getElementById("mow-btn")
 const pullBtn = document.getElementById("pull-btn")
 const taskList = document.getElementById("task-list")
-// const delBtn = document.getElementById("del-btn")
-const rmvWashBtn = document.getElementById("rmv-wash-btn")
-const rmvMowBtn = document.getElementById("rmv-mow-btn")
-const rmvPullBtn = document.getElementById("rmv-pull-btn")
+const delBtn = document.querySelectorAll("#task button")
 const totalEl = document.getElementById("total-el")
 const sendBtn = document.getElementById("send-btn")
 // Tracking tasks rendered
+// let tasksRender = {
+//   wash: false,
+//   mow: false,
+//   pull: false
+// }
 let washRender = false
 let mowRender = false
 let pullRender = false
+
+// washBtn.addEventListener("click", getTasksHtml(0))
+// mowBtn.addEventListener("click", getTasksHtml(1))
+// pullBtn.addEventListener("click", getTasksHtml(2))
+
+// function getTasksHtml(id) {
+//   if (!tasksRender[id]) {
+//     tasksRequested.unshift(services[id])
+//     totalAmount += tasksRequested[id].price
+//     notification.innerHTML = `<p class="notification-green">${tasksRequested[id].name} has been added successfully</p>`
+//     washRender = true
+//     renderServices()
+//   } else {
+//     notification.innerHTML = `<p class="notification-orange">You already have this on your list!</p>`
+//   }
+// }
 
 // Set event listener on buttons
 washBtn.addEventListener("click", function () {
@@ -60,41 +78,33 @@ pullBtn.addEventListener("click", function () {
   }
 })
 
-// // Set up remove btn
-// if (washRender) {
-//   // const delBtn = document.getElementById("del-btn")
-//   rmvWashBtn.addEventListener("click", function () {
-//     const indexOf = tasksRequested.indexOf(services[0])
-//     tasksRequested.splice(indexOf, 1)
-//     renderServices(tasksRequested)
-//   })
-// }
-
-// if (mowRender) {
-//   rmvMowBtn.addEventListener("click", function () {
-//     const indexOf = tasksRequested.indexOf(services[1])
-//     tasksRequested.splice(indexOf, 1)
-//     renderServices(tasksRequested)
-//   })
-// }
-
-// if (pullRender) {
-//   rmvPullBtn.addEventListener("click", function () {
-//     const indexOf = tasksRequested.indexOf(services[2])
-//     tasksRequested.splice(indexOf, 1)
-//     renderServices(tasksRequested)
-//   })
-// }
-
 sendBtn.addEventListener("click", function () {
-  tasksRequested = []
-  totalAmount = 0
-  notification.innerHTML = `<p class="notification-blue">Invoice has been sended successfully</p>`
-  washRender = false
-  pullRender = false
-  mowRender = false
-  renderServices(tasksRequested)
+  if (tasksRequested.length === 0) {
+    notification.innerHTML = `<p class="notification-red">You have no tasks on your list!</p>`
+  } else {
+    tasksRequested = []
+    totalAmount = 0
+    notification.innerHTML = `<p class="notification-blue">Invoice has been sended successfully</p>`
+    washRender = false
+    pullRender = false
+    mowRender = false
+    renderServices(tasksRequested)
+  }
 })
+
+function removeTask(id) {
+  let index = tasksRequested.findIndex((task) => task.id === id)
+  if (index === 0) {
+    washRender = false
+  } else if (index === 1) {
+    mowRender = false
+  } else if (index === 2) {
+    pullRender = false
+  }
+  totalAmount -= tasksRequested[index].price
+  tasksRequested.splice(index, 1)
+  renderServices(tasksRequested)
+}
 
 function renderServices() {
   let listService = ""
@@ -103,7 +113,7 @@ function renderServices() {
       <tr>
         <td class="table-left">
           ${tasksRequested[i].name}
-          <button class="remove-btn" id"delBtn">Remove</button>
+          <button class="remove-btn" id"delBtn" onclick="removeTask(${tasksRequested[i].id})">Remove</button>
         </td>
         <td class="table-right"><span class="currency">$</span>${tasksRequested[i].price}</td>
       </tr>
@@ -112,5 +122,3 @@ function renderServices() {
   taskList.innerHTML = listService
   totalEl.innerHTML = "$" + totalAmount
 }
-
-console.log(washRender)
